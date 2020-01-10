aa2b7eb3a61b9f594b7cb7ea2b4a2854ac6fc9da
Votre Nom <odilecoutiez@gmail.com>
2020-01-10 16:41:48 +0100

    Les fichier parent pour le host de la partie

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Partie-VI</title>
+    </head>
+    <body>
+        <a href="exo-1/index.php">Exercice-1</a><br />
+        <a href="exo-2/index.php">Exercice-2</a><br />
+        <a href="exo-3/index.php">Exercice-3</a><br />
+        <a href="exo-4/index.php">Exercice-4</a><br />
+        <a href="exo-5/index.php">Exercice-5</a><br />
+        <a href="exo-6/index.php">Exercice-6</a><br />
+    </body>
+</html>
